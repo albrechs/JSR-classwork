cbf8a31f82f0381eb2c584294b99ac1f78a37e58
Trevor Albrecht <talbrecht@draftkings.com>
2017-05-22 23:52:18 -0400

    got the button working

--- class02-madlib/madlib-with-loops/js/madlib-console.js
+++ class02-madlib/madlib-with-loops/js/madlib-console.js
@@ -5,3 +5,10 @@ var random1 = Math.floor((Math.random() * startupX.length));
 var random2 = Math.floor((Math.random() * startupY.length));
 
 console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+
+function newStartup(){
+		var startupNew = document.createTextNode('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
+		document.getElementById('xForY').appendChild(startupNew);
+}
+
+
