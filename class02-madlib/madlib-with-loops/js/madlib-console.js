26cd9f3d16447180729dd491618c65486846c47a
Trevor Albrecht <talbrecht@draftkings.com>
2017-05-23 00:16:26 -0400

    clears old startup before displaying new

--- class02-madlib/madlib-with-loops/js/madlib-console.js
+++ class02-madlib/madlib-with-loops/js/madlib-console.js
@@ -5,14 +5,22 @@ function pickRandoms(array){
 	var random1 = Math.floor((Math.random() * array.length));
 	// var random2 = Math.floor((Math.random() * startupY.length));
 
-	return array[random1]
+	return array[random1];
 }
 
 // console.log('A startup that is ' + startupX[random1] + ', but for ' + startupY[random2]);
 
 function newStartup(){
-		var startupNew = document.createTextNode('A startup that is ' + pickRandoms(startupX) + ', but for ' + pickRandoms(startupY));
+	var startupNew = document.createTextNode('A startup that is ' + pickRandoms(startupX) + ', but for ' + pickRandoms(startupY));
+	console.log('A startup that is ' + pickRandoms(startupX) + ', but for ' + pickRandoms(startupY));
+
+	if (document.getElementById('xForY').childNodes.length === 0) {
 		document.getElementById('xForY').appendChild(startupNew);
+	}
+	else {
+		document.getElementById('xForY').removeChild(document.getElementById('xForY').childNodes[0]);
+		document.getElementById('xForY').appendChild(startupNew);
+	}
 }
 
 
