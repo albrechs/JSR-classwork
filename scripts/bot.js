c696e97d7257696b88c7d71368dc4d934ddcde0d
Trevor Albrecht <talbrecht@draftkings.com>
2017-06-05 19:57:11 -0400

    case sensitivity

--- scripts/bot.js
+++ scripts/bot.js
@@ -3,7 +3,11 @@ module.exports = function(robot) {
     return res.send("Hi there!");
   });
 
-  robot.hear(/What are your Prime Directives?/, function(res) {
+  //robot.hear(/What are your Prime Directives?/, function(res) {
+  //	return res.send("Serve the public trust, protect the innocent, uphold the law.")
+  //});
+
+  robot.hear(/(W|w)hat are your (P|p)rime (D|d)irectives?/, function(res) {
   	return res.send("Serve the public trust, protect the innocent, uphold the law.")
   });
 }
