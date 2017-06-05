e79f7f7a4eac7259cd20334128e5825c5ac4568e
Trevor Albrecht <talbrecht@draftkings.com>
2017-06-05 19:46:46 -0400

    adding another function

--- scripts/bot.js
+++ scripts/bot.js
@@ -1,5 +1,9 @@
 module.exports = function(robot) {
-    robot.hear(/Hello!/, function(res) {
-      return res.send("Hi there!");
-    });
+  robot.hear(/Hello!/, function(res) {
+    return res.send("Hi there!");
+  });
+
+  robot.hear(/whattup?/, function(res) {
+  	return res.send("not much, you?")
+  });
 }
