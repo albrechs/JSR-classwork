e362b31af4f75f3baed535b7dc86763a51e1813a
Trevor Albrecht <talbrecht@draftkings.com>
2017-06-05 21:04:23 -0400

    fixing stuff

--- scripts/bot.js
+++ scripts/bot.js
@@ -1,24 +1,24 @@
 module.exports = function(robot) {
-  robot.hear(/Hello!/, function(res) {
+  robot.hear(/Hello!/i, function(res) {
     return res.send("Hi there!");
   });
 
-  robot.hear(/how are you?/, function(res) {
+  robot.hear(/how are you?/i, function(res) {
   	return res.send("Excuse me. I have to go. Somewhere there is a crime happening.");
   });
 
-  robot.hear(/what are your (/prime directives?/, function(res) {
+  robot.hear(/what are your prime directives?/i, function(res) {
   	return res.send("Serve the public trust, protect the innocent, uphold the law.");
   });
 
-  robot.hear(/pick a card!/, function(res) {
+  robot.respond(/pick a card/i, function(res) {
   	var cards = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"];
   	var suits = ["Hearts", "Spades", "Clubs", "Diamonds"];
 
-  	var cRand = Math.floor((Math.random() * 13) - 1);
-  	var sRand = Math.floor((Math.random() * 4) - 1);
+  	var cRand = Math.floor(Math.random() * 13);
+  	var sRand = Math.floor(Math.random() * 4);
 
-  	return cards[cRand] + " of " + suits[sRand]
+  	return res.reply(cards[cRand] + " of " + suits[sRand])
   });
 
   //robot.hear(/ /i, function(res) {
