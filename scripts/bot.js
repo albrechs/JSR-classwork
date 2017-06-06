26e6fae0fca333d378d1f9d26eb371d69d10d638
Trevor Albrecht <talbrecht@draftkings.com>
2017-06-05 20:24:16 -0400

    3rd function

--- scripts/bot.js
+++ scripts/bot.js
@@ -3,9 +3,9 @@ module.exports = function(robot) {
     return res.send("Hi there!");
   });
 
-  //robot.hear(/What are your Prime Directives?/, function(res) {
-  //	return res.send("Serve the public trust, protect the innocent, uphold the law.")
-  //});
+  robot.hear(/(H|h)ow are you?/, function(res) {
+  	return res.send("Excuse me. I have to go. Somewhere there is a crime happening.")
+  });
 
   robot.hear(/(W|w)hat are your (P|p)rime (D|d)irectives?/, function(res) {
   	return res.send("Serve the public trust, protect the innocent, uphold the law.")
