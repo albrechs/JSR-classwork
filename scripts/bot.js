261cc62fcb3c6e2fb145dd550b099627aeed9ad5
Trevor Albrecht <talbrecht@draftkings.com>
2017-06-07 00:47:22 -0400

    -

--- scripts/bot.js
+++ scripts/bot.js
@@ -21,7 +21,7 @@ module.exports = function(robot) {
   	return res.reply(cards[cRand] + " of " + suits[sRand])
   });
 
-  robot.hear(/do you know (.*)?/i, function(res) {
+  robot.hear(/do you know (.*)/i, function(res) {
   	var inputName = res.match[1];
   	var knownPeople = ["Bert", "Kirby", "Keenan" , "Sidney", "Tom", "Carter", "Bernie", "Eduardo", "Pierre", "Scott"];
 
