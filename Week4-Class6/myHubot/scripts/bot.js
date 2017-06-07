393c025fe17833596665ec0ac78e6177d43e8958
Trevor Albrecht <talbrecht@draftkings.com>
2017-06-07 19:00:34 -0400

    hubot project

---
+++ Week4-Class6/myHubot/scripts/bot.js
@@ -0,0 +1,40 @@
+module.exports = function(robot) {
+  robot.hear(/Hello!/i, function(res) {
+    return res.send("Hi there!");
+  });
+
+  robot.hear(/how are you?/i, function(res) {
+  	return res.send("Excuse me. I have to go. Somewhere there is a crime happening.");
+  });
+
+  robot.hear(/what are your prime directives?/i, function(res) {
+  	return res.send("Serve the public trust, protect the innocent, uphold the law.");
+  });
+
+  robot.respond(/pick a card/i, function(res) {
+  	var cards = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"];
+  	var suits = ["Hearts", "Spades", "Clubs", "Diamonds"];
+
+  	var cRand = Math.floor(Math.random() * 13);
+  	var sRand = Math.floor(Math.random() * 4);
+
+  	return res.reply(cards[cRand] + " of " + suits[sRand])
+  });
+
+  robot.hear(/do you know (.*)/i, function(res) {
+  	var inputName = res.match[1];
+  	var knownPeople = ["Bert", "Kirby", "Keenan" , "Sidney", "Tom", "Carter", "Bernie", "Eduardo", "Pierre", "Scott"];
+
+  	if (knownPeople.includes(inputName)) {
+  		return res.send("Yes, of couse I know " + inputName)
+  	}
+  	else {
+  		return res.send("Nope, never heard of anyone called " + inputName)
+  	}
+  });
+
+  robot.hear(/who are your friends?/i, function(res) {
+  	var knownPeople = ["Bert", "Kirby", "Keenan" , "Sidney", "Tom", "Carter", "Bernie", "Eduardo", "Pierre", "Scott"];
+    return res.send("I have many, but my best friends are " + knownPeople.join(', ') + ".")
+  });
+}
