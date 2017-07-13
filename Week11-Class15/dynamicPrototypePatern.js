994844b4f7bf610ac95995f5f75e0437b78706bc
Trevor Albrecht <talbrecht@draftkings.com>
2017-07-12 20:47:16 -0400

    -

---
+++ Week11-Class15/dynamicPrototypePatern.js
@@ -0,0 +1,56 @@
+// -------------------------
+// Dynamic Prototype Pattern
+// -------------------------
+
+// constructor function to create car objects
+/*function Car(make, model, year) {
+    this.make   = make;
+    this.model  = model;
+    this.year   = year;
+
+    // constructor prototype to share properties and methods
+    if ( typeof this.sayCar !== "function" ) {
+        Car.prototype.sayCar = function() {
+            alert('I have a ' + this.year + ' ' + this.make + ' ' + this.model + '.');    
+        }        
+    }
+}
+
+
+// create 2 car objects for John and Jane
+var johnCar = new Car('Ford', 'F150', '2011'),
+    janeCar = new Car('Audi', 'A4', '2007');
+
+// call method on Jane's car
+janeCar.sayCar();*/
+
+function Bear(name, type, age) {
+  this.name = name;
+  this.type = type;
+  this.age = age;
+
+  if (typeof this.greet !== 'function') {
+    Bear.prototype.greet = function() {
+      console.log('Hi, I am ' + this.name + ' the ' + this.type + ' bear. I am ' + this.age + ' years old.');
+    }
+  }
+
+  if (typeof this.growl !== 'function') {
+    Bear.prototype.growl = function() {
+      console.log('grrrrrrr');
+    }
+  }
+}
+
+var bob = new Bear('Bob', 'grizzly', 12);
+var ben = new Bear('Ben', 'brown', 3);
+var tom = new Bear('Tom', 'polar', 8);
+
+bob.greet();
+bob.growl();
+ben.greet();
+ben.growl();
+tom.greet();
+tom.growl();
+
+
